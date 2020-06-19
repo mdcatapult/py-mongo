3e5268907f420a5da7ffff3ad3cb753d043123d9
Mark Laing <mark.laing@md.catapult.org.uk>
2020-06-19 09:26:02 +0100

    Increments patch version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='klein_mongo',
-      version='1.0.1',
+      version='1.0.2',
       description='MongoDB integration',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein_mongo',
       author='Matt Cockayne',
