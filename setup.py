91a49fbe2c4f8be177851730680b71d2fd1ba505
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-03 10:27:28 +0100

    Adds python ci image and release process.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
+__version__ = ''
+exec(open("./src/version.py").read())
+if __version__ == '':
+    raise RuntimeError("unable to find application version")
+
 setup(name='klein_mongo',
-      version='1.0.2',
+      version=__version__,
       description='MongoDB integration',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein_mongo',
       author='Matt Cockayne',
